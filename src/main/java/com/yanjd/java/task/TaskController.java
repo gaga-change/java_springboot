a6dd46b39e4bad4f5376afa36c9a4c73a7d6f474
严俊东 <1172482914@qq.com>
2018-06-29 23:27:51 +0800

    删除 & 修改 & 指定查询 协议

--- src/main/java/com/yanjd/java/task/TaskController.java
+++ src/main/java/com/yanjd/java/task/TaskController.java
@@ -1,10 +1,7 @@
 package com.yanjd.java.task;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,23 +13,68 @@ public class TaskController {
 
     /**
      * 查询所有任务
+     *
      * @return
      */
     @GetMapping(value = "/task")
-    public List<Task> findAll () {
+    public List<Task> findAll() {
         return taskRepository.findAll();
     }
 
+
+    /**
+     * 获取任务详情
+     * @param id
+     * @return
+     */
+    @GetMapping(value = "/task/{id}")
+    public Task getTaskOne(
+            @PathVariable("id") Integer id
+    ) {
+        return taskRepository.findById(id).orElse(null);
+    }
+
+
     /**
      * 添加任务
+     *
      * @param content
      * @return
      */
     @PostMapping(value = "/task")
-    public Task saveTask (
+    public Task saveTask(
             @RequestParam("content") String content
     ) {
         Task task = new Task();
+        task.setContent(content);
+        return taskRepository.save(task);
+    }
+
+    /**
+     * 删除任务
+     *
+     * @param id
+     */
+    @DeleteMapping(value = "/task/{id}")
+    public void deleteTask(
+            @PathVariable("id") Integer id
+    ) {
+        taskRepository.deleteById(id);
+    }
+
+    /**
+     * 修改任务
+     *
+     * @param id
+     * @param content
+     * @return
+     */
+    @PutMapping(value = "/task/{id}")
+    public Task modifyTask(
+            @PathVariable("id") Integer id,
+            @RequestParam("content") String content
+    ) {
+        Task task = taskRepository.findById(id).orElse(null);
         task.setContent(content);
         return taskRepository.save(task);
     }
