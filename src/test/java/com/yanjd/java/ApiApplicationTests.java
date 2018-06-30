46a9110e51295c3d90f24767b8f2ff479ba6bf2c
严俊东 <1172482914@qq.com>
2018-06-30 17:39:20 +0800

    修改测试类

--- src/test/java/com/yanjd/java/ApiApplicationTests.java
+++ src/test/java/com/yanjd/java/ApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.yanjd.girl;
+package com.yanjd.java;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,8 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class GirlApplicationTests {
-
+public class ApiApplicationTests {
     @Test
     public void contextLoads() {
     }
